c8893f9446621837de223c18948ec6843cc85563
dvinix <gargdivyanshu4152@gmail.com>
2026-06-06 14:41:42 +0530

    Update the Base URL

--- app/core/config.py
+++ app/core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
     # Application
     app_host: str = Field(default="127.0.0.1", alias="APP_HOST")
     app_port: int = Field(default=8000, alias="APP_PORT")
-    base_url: str = Field(default="http://localhost:8000", alias="BASE_URL")
+    base_url: str = Field(alias="BASE_URL")
     
     # CORS
     cors_origins: str = Field(
